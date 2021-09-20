e2b956d99784dedcb192b8dfd90ef1a4e7251723
ananyo141 <ananyo141@gmail.com>
2021-09-20 22:52:43 +0530

    add database gui script

---
+++ Database/peoplegui.py
@@ -0,0 +1,11 @@
+"""Implement a GUI for viewing and updating records from shelve"""
+
+from tkinter import *
+from tkinter import showerror
+import shelve
+
+shelvename = 'class-shelve.auto'
+fieldnames = ('name', 'age', 'job', 'pay')
+
+def makeWidgets():
+    pass
